8509a711ff820fd67a5447c5a024a7a2264ca6ab
praveensingh1999 <praveen969500@gmail.com>
2025-11-14 18:57:26 +0530

    mongodb.

---
+++ Day_4/intro.js
@@ -0,0 +1,20 @@
+//  database 
+// database are structured system for starting, managing and retriving data effieciently.
+
+// categories based on how they store the data
+//1, Relational (sql database)
+// 2. Not realtional Database  (mongodb)
+
+// realtional database are store in the form of table where nonrelational database are store in the form of hireacy.
+//in realtional databases once we define schema then we can not change it.
+
+
+// data migration - for example we have one table it conatin column like name, roll no, age and i want to add one more column address then we create new
+// table and add column name , rollno ,age, address that process is called data migration.
+
+// there is no schema in non realtional databases. and store data in json formate
+
+// data modeling
+// when wwe creating a database inside a table there is schema that will be inside the databse only
+// but in nosql we validate the data on the server itself that is called data modeling.
+
