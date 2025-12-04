deb1ea062d4e26e451896f857dc09a1ac4c6f317
Praveen Singh <57175335+praveensingh1999@users.noreply.github.com>
2025-12-04 19:24:37 +0530

    Update intro.js

--- Day_5/intro.js
+++ Day_5/intro.js
@@ -3,6 +3,3 @@
 //ODM makes working with MongoDB documents feel like working with normal Ja
 
 
-//praveen9695003121_db_user
-//K5W3m0WC4gsLIOJD
-//mongodb+srv://praveen9695003121_db_user:K5W3m0WC4gsLIOJD@cluster0.8jhmep2.mongodb.net/?appName=Cluster0
